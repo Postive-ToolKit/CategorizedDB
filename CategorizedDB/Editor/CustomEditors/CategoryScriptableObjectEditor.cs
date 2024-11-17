48fbc33b8b2aecfd31ac315dcdffdbbf66cf6639
postive12 <postive12@gmail.com>
2024-11-18 04:41:18 +0900

    Add Odin Compatibility

--- CategorizedDB/Editor/CustomEditors/CategoryScriptableObjectEditor.cs
+++ CategorizedDB/Editor/CustomEditors/CategoryScriptableObjectEditor.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections.Generic;
 using Postive.CategorizedDB.Runtime.Categories;
 using UnityEditor;
+using UnityEngine;
+#if ODIN_INSPECTOR
+using Sirenix.OdinInspector.Editor;
+#endif
 
 namespace Postive.CategorizedDB.Editor.CustomEditors
 {
@@ -8,39 +12,59 @@ namespace Postive.CategorizedDB.Editor.CustomEditors
     public class CategoryScriptableObjectEditor : UnityEditor.Editor
     {
         private List<string> _defaultProperties = new List<string>() { "m_Script", "_parentGUID", "_name", "_path" };
+        private CategoryScriptableObject _categoryScriptableObject;
+#if ODIN_INSPECTOR
+        private PropertyTree _propertyTree;
+#endif
+        private void OnEnable() {
+            _categoryScriptableObject = (CategoryScriptableObject) target;
+#if ODIN_INSPECTOR
+            _propertyTree = PropertyTree.Create(serializedObject.targetObject);
+#endif
+        }
         public override void OnInspectorGUI() {
-            //get object
-            CategoryScriptableObject categoryScriptableObject = (CategoryScriptableObject) target;
             serializedObject.Update();
             EditorGUILayout.PropertyField(serializedObject.FindProperty("_parentGUID"));
             //show path
-            EditorGUILayout.LabelField("Path", categoryScriptableObject.GetPath());
+            EditorGUILayout.LabelField("Path", _categoryScriptableObject.GetPath());
             EditorGUILayout.PropertyField(serializedObject.FindProperty("_name"));
-            serializedObject.ApplyModifiedProperties();
+            if (GUI.changed) {
+                serializedObject.ApplyModifiedProperties();
+            }
+            
+            
+            EditorGUILayout.LabelField("Contents", EditorStyles.boldLabel);
+            //draw thin line
+            var rect = EditorGUILayout.GetControlRect(false, 1);
+            rect.height = 1;
+            EditorGUI.DrawRect(rect, new Color(0.5f, 0.5f, 0.5f, 1));
+            
             ShowOtherProperties();
         }
-        public virtual void ShowOtherProperties()
-        {
-            bool isContentExist = false;
-            //show other properties
+        public virtual void ShowOtherProperties() {
+#if ODIN_INSPECTOR
+            _propertyTree.UpdateTree();
+            var tree = _propertyTree.EnumerateTree(true);
+            _propertyTree.BeginDraw(false);
+            foreach (var property in tree) {
+                if (!_defaultProperties.Contains(property.Name)) {
+                    property.Draw();
+                }
+            }
+            _propertyTree.EndDraw();
+            _propertyTree.ApplyChanges();
+            
+#else
             serializedObject.Update();
             SerializedProperty property = serializedObject.GetIterator();
             property.NextVisible(true);
             while (property.NextVisible(false)) {
                 if (!_defaultProperties.Contains(property.name)) {
-                    if (!isContentExist) {
-                        isContentExist = true;
-                        EditorGUILayout.LabelField("Contents", EditorStyles.boldLabel);
-                        //draw thin line
-                        var rect = EditorGUILayout.GetControlRect(false, 1);
-                        rect.height = 1;
-                        EditorGUI.DrawRect(rect, new UnityEngine.Color(0.5f, 0.5f, 0.5f, 1));
-                        
-                    }
                     EditorGUILayout.PropertyField(property, true);
                 }
             }
             serializedObject.ApplyModifiedProperties();
+#endif
         }
     }
 }
