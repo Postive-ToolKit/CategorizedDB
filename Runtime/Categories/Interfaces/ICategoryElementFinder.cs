47f5f39c9777e08d4749e7008b7ab76c63c74da2
postive12 <postive12@gmail.com>
2025-02-27 00:18:58 +0900

    Update ICategoryElementFinder.cs

--- Runtime/Categories/Interfaces/ICategoryElementFinder.cs
+++ Runtime/Categories/Interfaces/ICategoryElementFinder.cs
@@ -6,7 +6,9 @@ namespace Postive.CategorizedDB.Runtime.Categories.Interfaces
     public interface ICategoryElementFinder
     {
         public List<CategoryElement> Elements { get; }
+#if UNITY_EDITOR
         public void CreateData(Type type,Category selectedCategory = null);
-        public void RemoveData(CategoryElement data);
+        public void RemoveData(CategoryElement data); 
+#endif
     }
 }
