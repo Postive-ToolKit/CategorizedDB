a815856c4e5015eb3230bccef1de7e284851c6db
postive12 <postive12@gmail.com>
2025-03-30 13:05:57 +0900

    fix: change default view to show category first

--- Editor/CustomEditors/Native/CategorizedDBEditor/TreeBuilders/DefaultTreeViewItemBuilder.cs
+++ Editor/CustomEditors/Native/CategorizedDBEditor/TreeBuilders/DefaultTreeViewItemBuilder.cs
@@ -17,8 +17,14 @@ namespace Postive.CategorizedDB.Editor.CustomEditors.Native.CategorizedDBEditor.
         {
             _currentId = 0;
             List<TreeViewItemData<ICategoryElement>> rootItems = new List<TreeViewItemData<ICategoryElement>>();
-            foreach (var data in elements.FindAll(x => string.IsNullOrEmpty(x.ParentGUID))) {
-                rootItems.Add(BuildTreeItem(data, elements));
+            var rootElements = elements.FindAll(x => string.IsNullOrEmpty(x.ParentGUID));
+            var rootCategories = rootElements.FindAll(x => x is Category);
+            var rootCategoryElements = rootElements.FindAll(x => x is CategoryElement);
+            foreach (var rootCategory in rootCategories) {
+                rootItems.Add(BuildTreeItem(rootCategory, elements));
+            }
+            foreach (var rootCategoryElement in rootCategoryElements) {
+                rootItems.Add(BuildTreeItem(rootCategoryElement, elements));
             }
             return rootItems;
         }
@@ -31,8 +37,13 @@ namespace Postive.CategorizedDB.Editor.CustomEditors.Native.CategorizedDBEditor.
 
             if (children.Count > 0) {
                 var childItems = new List<TreeViewItemData<ICategoryElement>>();
-                foreach (var child in children) {
-                    childItems.Add(BuildTreeItem(child, elements));
+                var categories = children.FindAll(x => x is Category);
+                var categoryElements = children.FindAll(x => x is CategoryElement);
+                foreach (var childCategory in categories) {
+                    childItems.Add(BuildTreeItem(childCategory, elements));
+                }
+                foreach (var childCategoryElement in categoryElements) {
+                    childItems.Add(BuildTreeItem(childCategoryElement, elements));
                 }
                 return new TreeViewItemData<ICategoryElement>(currentCreatedId, data, childItems);
             }
